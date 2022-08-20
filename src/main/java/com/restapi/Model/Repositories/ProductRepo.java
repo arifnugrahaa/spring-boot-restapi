0677d5db077fa0d35c8cdcb9a46ac5e05da33acd
arifnugrahaa <arifnugraha1243@gmail.com>
2022-08-20 22:16:00 +0700

    [membuat services]

--- src/main/java/com/restapi/Model/Repositories/ProductRepo.java
+++ src/main/java/com/restapi/Model/Repositories/ProductRepo.java
@@ -1,9 +1,12 @@
 package com.restapi.Model.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.restapi.Model.Entities.Product;
 
 public interface ProductRepo extends CrudRepository<Product, Long> {
     
+    List<Product> findByNameContains(String name);
 }
---
+++ src/main/java/com/restapi/Services/ProductService.java
@@ -0,0 +1,39 @@
+package com.restapi.Services;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.restapi.Model.Entities.Product;
+import com.restapi.Model.Repositories.ProductRepo;
+
+@Service
+@Transactional
+public class ProductService {
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    public Product save(Product product){
+        return productRepo.save(product);
+    }
+
+    public Product findOne(Long id){
+        return productRepo.findById(id).get();
+    }
+
+    public Iterable<Product> findAll(){
+        return productRepo.findAll();
+    }
+
+    public void removeOne(Long id){
+        productRepo.deleteById(id);
+    }
+
+    public List<Product> findByName(String name){
+        return productRepo.findByNameContains(name);
+    }
+}
