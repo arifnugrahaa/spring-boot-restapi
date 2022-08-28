ed1d7cab828f0ecd4ba30e744082ca056612a894
arifnugrahaa <arifnugraha1243@gmail.com>
2022-08-28 22:16:18 +0700

    [swagger api documentation]

---
+++ src/main/java/com/restapi/Swagger/SwaggerConfiguration.java
@@ -0,0 +1,43 @@
+package com.restapi.Swagger;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.restapi.Controller"))
+            .paths(PathSelectors.any())
+            .build()
+            .apiInfo(apiInfo());
+    }
+
+    public ApiInfo apiInfo(){
+        ApiInfo apiInfo = new ApiInfo(
+            "My Demo API",
+            "Ini adalah demo API dengan SpringBoot",
+            "v1.0",
+            "Terms of service",
+            new Contact("Arif Nugraha", null, null),
+            "License of API",
+            "Apache License",
+            Collections.emptyList()
+        );
+        return apiInfo;
+    }
+
+}
